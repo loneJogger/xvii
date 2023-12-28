c7c6a797782994f555b1a0d2ca965a3470504feb
Helen Tremarche <helen@Helens-MacBook-Air.local>
2023-12-27 19:23:02 -0500

    tutorial 1 finished

--- client/src/pages/Test.js
+++ client/src/pages/Test.js
@@ -1,11 +1,29 @@
-import Game from '../components/HWGame'
+import { useState } from 'react'
+import HWGame from '../components/phaser/HelloWorld'
+import Tu1Game from '../components/phaser/Tutorial1'
 
 const Test = () => {
 
+    const [ currentPhaser, setCurrentPhaser ] = useState('')
 
     return (
         <div>
-            <Game />
+            <h1>Phaser practice</h1>
+            <div>
+                <select onChange={(e) => setCurrentPhaser(e.target.value)}>
+                    <option value={'none'}>none</option>
+                    <option value={'hwGame'}>hello world</option>
+                    <option value={'tu1Game'}>tutorial 1</option>
+                </select>
+            </div>
+            <div>
+                {(currentPhaser === 'hwGame') && (
+                    <HWGame />
+                )}
+                {(currentPhaser === 'tu1Game') && (
+                    <Tu1Game />
+                )}
+            </div>
         </div>
     )
 }
